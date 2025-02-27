f94fa5e19b488c07c58d737b3482695a37ed9d92
Danny Ruffolo <druffolo10@gmail.com>
2025-02-27 10:00:47 -0600

    updated

--- xinu-hw5/include/version.h
+++ xinu-hw5/include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (nezhaobj) #54 (smrusek@morbius.mscsnet.mu.edu) Tue Feb 11 09:53:04 PM CST 2025"
+#define VERSION "(Embedded Xinu) (nezhaobj) #58 (fruffolo1@morbius.mscsnet.mu.edu) Tue Feb 25 06:25:36 PM CST 2025"

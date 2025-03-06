25baefaea371b9095dc6d8ea108fbb45741fc280
Danny Ruffolo <druffolo10@gmail.com>
2025-03-05 21:30:11 -0600

    updated dispatch.c

--- xinu-hw5/system/dispatch.c
+++ xinu-hw5/system/dispatch.c
@@ -1,4 +1,13 @@
 /**
+ * COSC 3250 - Project 5
+ * This is a function to provide memory protection  
+ * @author [Sam Mrusek and Danny Ruffolo]
+ * Instructor [Dr.Brylow]
+ * TA-BOT:MAILTO [samantha.mrusek@marquette.edu danny.ruffolo@marquette.edu]
+ */
+
+
+/**
  * @file dispatch.c
  * @provides dispatch
  *
@@ -20,6 +29,17 @@
 void dispatch(ulong cause, ulong val, ulong *frame, ulong *program_counter) {
     ulong swi_opcode;
     
+        /**
+        * TODO:
+        * Check to ensure the trap is an environment call from U-Mode
+        * Find the system call number that's triggered
+        * Pass the system call number and any arguments into syscall_dispatch.
+        * Make sure to set the return value in the appropriate spot.
+        * Update the program counter appropriately with set_sepc
+        *
+        * If the trap is not an environment call from U-Mode call xtrap
+        */
+
     if((long)cause > 0) {
         cause = cause << 1;
         cause = cause >> 1;
@@ -38,17 +58,5 @@ void dispatch(ulong cause, ulong val, ulong *frame, ulong *program_counter) {
 		// If not U-mode, call xtrap function
 		xtrap(frame, cause, val, program_counter); 
 	}
-    
-	/**
-	* TODO:
-	* Check to ensure the trap is an environment call from U-Mode
-	* Find the system call number that's triggered
-	* Pass the system call number and any arguments into syscall_dispatch.
-	* Make sure to set the return value in the appropriate spot.
-	* Update the program counter appropriately with set_sepc
-	*
-	* If the trap is not an environment call from U-Mode call xtrap
-	*/
     }
 }
-
